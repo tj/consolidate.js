c1e768fc792e4e972b7c841a8c8d6d5a69faeeb2
TJ Holowaychuk <tj@vision-media.ca>
2011-11-19 23:36:21 -0800

    Added jqtpl

--- test/consolidate.js
+++ test/consolidate.js
@@ -3,4 +3,5 @@ require('./shared').test('jade');
 require('./shared').test('ejs');
 require('./shared').test('swig');
 require('./shared').test('jazz');
+require('./shared').test('jqtpl');
 require('./shared').test('liquor');
