b75aad59abdb1e1753ff7bc92deeeec24b02646b
TJ Holowaychuk <tj@vision-media.ca>
2011-12-03 10:12:07 -0800

    express 3.x example

---
+++ examples/express.js
@@ -0,0 +1,31 @@
+
+// npm install express
+
+var express = require('../../express')
+  , cons = require('../')
+  , app = express();
+
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+var users = [];
+users.push({ name: 'tobi' });
+users.push({ name: 'loki' });
+users.push({ name: 'jane' });
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Consolidate.js'
+  });
+});
+
+app.get('/users', function(req, res){
+  res.render('users', {
+    title: 'Users',
+    users: users
+  });
+});
+
+app.listen(3000);
+console.log('Express server listening on port 3000');
