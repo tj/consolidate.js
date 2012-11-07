e53c359fafea49ac9fdf8dcf979db03c5d8ef905
simov <ssimo@abv.bg>
2012-11-07 10:39:01 +0200

    Added tests for cached partials and partial from string. The tests are for Hogan, Mustache and Handlebars!

--- test/shared/partials.js
+++ test/shared/partials.js
@@ -23,8 +23,35 @@ exports.test = function(name) {
           done();
         });
       });
+      it('should support cached partials', function(done){
+        var path = 'test/fixtures/' + name + '/partials.' + name;
+        var locals = { user: user, cache: true, partials: { partial: 'user' } };
+        cons[name](path, locals, function(err, html){
+          if (err) return done(err);
+          fs.readFile = function(path){
+            done(new Error('fs.readFile() called with ' + path));
+          };
+          html.should.equal('<p>Tobi</p>');
+          locals = { user: user, cache: true, partials: { partial: 'user' } };
+          cons[name](path, locals, function(err, html){
+            if (err) return done(err);
+            html.should.equal('<p>Tobi</p>');
+            done();
+          });
+        });
+      });
+      it('should support rendering a partial from string', function(done){
+        var str = fs.readFileSync('test/fixtures/' + name + '/partials.' + name).toString();
+        var partialStr = fs.readFileSync('test/fixtures/' + name + '/user.' + name).toString();
+        var locals = { user: user, partials: { partial: partialStr } };
+        cons[name].render(str, locals, function(err, html){
+          if (err) return done(err);
+          html.should.equal('<p>Tobi</p>');
+          done();
+        });
+      });
     }
-    else {
+    if (name == 'dust') {
       it('should support rendering a partial', function(done){
         var str = fs.readFileSync('test/fixtures/' + name + '/user_partial.' + name).toString();
         var locals = { 
