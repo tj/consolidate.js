8f6402eaec7ea78cdf47970f4b781bb5f251b74c
Daniel Cherubini <daniel@cherubini.casa>
2016-12-06 10:04:23 +0100

    ready for pull request

--- examples/express.js
+++ examples/express.js
@@ -1,46 +1,28 @@
 // npm install express
 
-var express = require('express');
-var path = require('path');
-var cons = require('../');
-var app = express();
+var express = require('../../express')
+  , cons = require('../')
+  , app = express();
 
-app.engine('vue', cons.expressVue);
-app.set('view engine', 'vue');
-app.set('views', path.join(__dirname, '/views'));
-app.set('vue', {
-  componentsDir: path.join(__dirname, '/views/components'),
-  defaultLayout: 'layout'
-})
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 var users = [];
-users.push({ name: 'tobi', age: 12 });
-users.push({ name: 'loki', age: 14  });
-users.push({ name: 'jane', age: 16  });
+users.push({ name: 'tobi' });
+users.push({ name: 'loki' });
+users.push({ name: 'jane' });
 
 app.get('/', function(req, res){
   res.render('index', {
-    data: {
-        title: 'Express Vue',
-        message: 'Hello!',
-        users: users
-    },
-    vue: {
-        components: ['users', 'message']
-    }
+    title: 'Consolidate.js'
   });
 });
 
-app.get('/users/:userName', function(req, res){
-    var user = users.filter(function(item) {
-        return item.name === req.params.userName;
-    })[0];
-  res.render('user', {
-    data: {
-      title: 'Hello My Name is',
-      user: user
-    }
-
+app.get('/users', function(req, res){
+  res.render('users', {
+    title: 'Users',
+    users: users
   });
 });
 
