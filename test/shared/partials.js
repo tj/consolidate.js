ff1cabea92c8feee75a3818de008376cc7fd9d71
doowb <brian.woodward@gmail.com>
2018-03-19 19:49:42 -0400

    update underscore partial testing to match other partial testing

--- test/shared/partials.js
+++ test/shared/partials.js
@@ -14,7 +14,20 @@ exports.test = function(name) {
       fs.readFileSync = readFileSync;
     });
 
-    if (name === 'hogan' || name === 'mustache' || name === 'handlebars' || name === 'ractive') {
+    if (name === 'dust' || name === 'arc-templates') {
+      it('should support rendering a partial', function(done) {
+        var str = fs.readFileSync('test/fixtures/' + name + '/user_partial.' + name).toString();
+        var locals = {
+          user: user,
+          views: './test/fixtures/' + name
+        };
+        cons[name].render(str, locals, function(err, html) {
+          if (err) return done(err);
+          html.should.equal('<p>Tobi from partial!</p><p>Tobi</p>');
+          done();
+        });
+      });
+    } else {
       it('should support partials', function(done) {
         var path = 'test/fixtures/' + name + '/partials.' + name;
         var locals = { user: user, partials: { partial: 'user' } };
@@ -39,34 +52,6 @@ exports.test = function(name) {
         cons[name](path, locals, function(err, html) {
           if (err) return done(err);
           html.should.equal('<p>Tobi</p>');
-          done();
-        });
-      });
-    } else if (name === 'underscore') {
-      it('should support partials', function(done) {
-        var path = 'test/fixtures/' + name + '/partials.' + name;
-        var locals = {
-          user: user,
-          partials: {
-            partial: 'user_partial'
-          }
-        };
-        cons[name](path, locals, function(err, html) {
-          if (err) return done(err);
-          html.should.equal('<p>Tobi</p><p>Tobi from user_partial!</p>');
-          done();
-        });
-      });
-    } else {
-      it('should support rendering a partial', function(done) {
-        var str = fs.readFileSync('test/fixtures/' + name + '/user_partial.' + name).toString();
-        var locals = {
-          user: user,
-          views: './test/fixtures/' + name
-        };
-        cons[name].render(str, locals, function(err, html) {
-          if (err) return done(err);
-          html.should.equal('<p>Tobi from partial!</p><p>Tobi</p>');
           done();
         });
       });
